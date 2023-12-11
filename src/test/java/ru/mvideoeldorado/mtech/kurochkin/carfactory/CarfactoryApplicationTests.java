229ed330414b66ca5e458d4a522b9097447d1b14
Kirikura <kirikura.w@yandex.ru>
2023-12-12 00:38:16 +0300

    Initial commit

---
+++ src/test/java/ru/mvideoeldorado/mtech/kurochkin/carfactory/CarfactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mvideoeldorado.mtech.kurochkin.carfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarfactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
